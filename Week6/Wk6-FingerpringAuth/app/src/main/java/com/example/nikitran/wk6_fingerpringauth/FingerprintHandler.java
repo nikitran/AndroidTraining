a27faa5167a290c48dd6ad5b0f2820f2efe8eb78
nikitran <nikitran@davids-mbp-3.attlocal.net>
2017-03-06 21:52:25 -0500

    initial commit for FingerprintAuth

--- Week6/Wk6-FingerpringAuth/app/src/main/java/com/example/nikitran/wk6_fingerpringauth/FingerprintHandler.java
+++ Week6/Wk6-FingerpringAuth/app/src/main/java/com/example/nikitran/wk6_fingerpringauth/FingerprintHandler.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.os.CancellationSignal;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 /**
  * Created by nikitran on 2/28/17.
@@ -18,17 +19,42 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
     private CancellationSignal cancellationSignal;
     private Context context;
 
-    public FingerprintHandler(Context context){
+    public FingerprintHandler(Context context) {
         this.context = context;
     }
 
-    public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject){
+    // 1. initialize the cancellationSignal
+    // 2. check for permissions
+    // 3. manager.authenticate(cryptoObjet, cancellation Signal, flag, Authentication callback, handler);
+    public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
         cancellationSignal = new CancellationSignal();
 
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT != PackageManager.PERMISSION_GRANTED)){
-            return; //no permissions
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
 
-        manager.authenticate(cyr);
+    @Override
+    public void onAuthenticationError(int errMsgId, CharSequence errString) {
+        Toast.makeText(context, "Authentication error\n" + errString, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onAuthenticationFailed() {
+        Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        Toast.makeText(context, "Authentication succeeded.", Toast.LENGTH_SHORT).show();
     }
 }
--- Week6/Wk6-FingerpringAuth/app/src/main/java/com/example/nikitran/wk6_fingerpringauth/MainActivity.java
+++ Week6/Wk6-FingerpringAuth/app/src/main/java/com/example/nikitran/wk6_fingerpringauth/MainActivity.java
@@ -1,13 +1,135 @@
 package com.example.nikitran.wk6_fingerpringauth;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.KeyguardManager;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
 public class MainActivity extends AppCompatActivity {
+
+    private static final String KEY_NAME = "KEY_NAME_TEST";
+    private static final String TAG = "MainActivity";
+
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
+
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
+
+    private FingerprintManager.CryptoObject cryptoObject;
+
+    private Cipher cipher;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the system service for fingerpring and keyguard manager:
+        fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+        keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+        if (! keyguardManager.isKeyguardSecure())
+        {
+            Toast.makeText(this, "Lock screen security not enabled in Settings", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) !=
+                PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Fingerprint authentication permission not enabled", Toast.LENGTH_LONG).show();
+            finish();
+
+        }
+
+        if(!fingerprintManager.hasEnrolledFingerprints()){
+            Toast.makeText(this, "Register at least one fingerprint in Settings", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        generateKey();
+
+        if(cipherInit()){
+            cryptoObject = new FingerprintManager.CryptoObject(cipher);
+            FingerprintHandler helper = new FingerprintHandler(this);
+            helper.startAuth(fingerprintManager, cryptoObject);
+        } else {
+            Toast.makeText(this, "cipherInitFalse", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    protected void generateKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            keyGenerator = keyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException("Fail to get KeyGenerator instance", e);
+        }
+
+        try {
+            keyStore.load(null);
+            keyGenerator.init(new KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | CertificateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean cipherInit(){
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                    + KeyProperties.BLOCK_MODE_CBC + "/"
+                    + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME, null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+
 }
